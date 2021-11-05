594c6bb459aed0110e43402c5cf5ee97a41e8ef6
Diegoslourenco <diego.lourenco15@gmail.com>
2021-11-05 17:02:38 -0300

    add comments in main

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        //mut mean that the variable is mutable, the default is immutable
+        // mut means that the variable is mutable, the default is immutable
         // Instantiating a new string
         // :: means that new is a associated function of the String type
         // In resume, created a mutable variable that is currently bound to a new, empty instance of a String
@@ -22,6 +22,11 @@ fn main() {
             .read_line(&mut guess)  // receives wherever comes in the input and append it to the string guess
             .expect("Failed to read line");    // handling possible error, the string message will prompt in case of error
 
+        /*
+            converts string to 32-bit unsigned int
+                if it is possible to convert, return the int
+                otherwise, prompt an error
+         */
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -29,6 +34,7 @@ fn main() {
 
         println!("You guessed: {}", guess);
 
+        // compare the number in guess to the secret_number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
