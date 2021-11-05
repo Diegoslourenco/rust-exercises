a9f01f554396b3ceb71fe2abd76cdd28eb7be1d7
Diegoslourenco <diego.lourenco15@gmail.com>
2021-11-04 22:31:52 -0300

    remove target

--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Diego Lourenço\Desktop\rust-exercises\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-C:\Users\Diego Lourenço\Desktop\rust-exercises\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Diego\ Lourenço\Desktop\rust-exercises\guessing_game\target\debug\guessing_game.exe: C:\Users\Diego\ Lourenço\Desktop\rust-exercises\guessing_game\src\main.rs
